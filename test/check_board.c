cbb6b25dd40e1d3653f126c70d8f84c0a9ef185e
ojplg <lees@ripco.com>
2019-06-17 23:42:45 -0500

    First unit test working!

--- test/check_board.c
+++ test/check_board.c
@@ -1,10 +1,6 @@
 #include <check.h>
+#include <stdlib.h>
 #include "../src/board.h"
-
-int main(void)
-{
-    return 0;
-}
 
 START_TEST(test_init_board)
 {
@@ -20,3 +16,33 @@ START_TEST(test_init_board)
 }
 END_TEST
 
+Suite * board_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("Board");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_init_board);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+int main(void)
+{
+	int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = board_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
